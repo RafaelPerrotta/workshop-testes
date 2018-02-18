29ec979823fba05c0aabd43c3e6937279e5b163a
Rodrigo Caneppele <r.caneppele@gmail.com>
2018-02-18 16:32:37 -0300

    alterando testes para rodar no Chrome e adicionando sufixo Test no nome da classe

--- src/test/java/br/com/caelum/workshop/rh/cargo/CadastroCargoTest.java
+++ src/test/java/br/com/caelum/workshop/rh/cargo/CadastroCargoTest.java
@@ -8,42 +8,42 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CadastroCargo {
+public class CadastroCargoTest {
 	
 	private String urlSistema = "https://workshop-testes.herokuapp.com";
-	private WebDriver firefox;
+	private WebDriver chrome;
 	
 	//metodo com @Before é executado antes de cada um dos testes
 	@Before
 	public void before() {
-		//indica o local do driver do firefox:
-		System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/firefox/windows/geckodriver");
+		//indica o local do driver do chrome:
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chrome/windows/chromedriver.exe");
 		
-		//Abre o Firefox:
-		firefox = new FirefoxDriver();
+		//Abre o Chrome:
+		chrome = new ChromeDriver();
 	}
 	
 	//metodo com @After é executado apos cada um dos testes
 	@After
 	public void after() {
-		firefox.close();
+		chrome.close();
 	}
 	
 	//metodo pra isolar o codigo de logar no sistema e evitar que esse trecho de codigo se repita em todos os metodos de teste
 	private void logarNoSistema() {
 		//navega pra pagina do login do sistema:
-		firefox.navigate().to(urlSistema + "/login");
+		chrome.navigate().to(urlSistema + "/login");
 		
 		//encontra o campo de login e preenche com rh:
-		firefox.findElement(By.name("username")).sendKeys("rh");
+		chrome.findElement(By.name("username")).sendKeys("rh");
 		
 		//encontra o campo de senha e preenche com 123456:
-		firefox.findElement(By.name("password")).sendKeys("123456");
+		chrome.findElement(By.name("password")).sendKeys("123456");
 		
 		//encontra o botao de login e clica nele:
-		firefox.findElement(By.cssSelector(".btn-primary")).click();
+		chrome.findElement(By.cssSelector(".btn-primary")).click();
 	}
 	
 	@Test
@@ -52,22 +52,22 @@ public class CadastroCargo {
 		logarNoSistema();
 		
 		//navega pra pagina de cadastro de cargos:
-		firefox.navigate().to(urlSistema + "/cargos/form");
+		chrome.navigate().to(urlSistema + "/cargos/form");
 		
 		//encontra o campo 'Nome' e preenche com 'Cargo teste':
-		firefox.findElement(By.name("nome")).sendKeys("Cargo teste");
+		chrome.findElement(By.name("nome")).sendKeys("Cargo teste");
 		
 		//encontra o campo 'Salario Minimo' e preenche com '500,00':
-		firefox.findElement(By.name("faixaSalarial.salarioMinimo")).sendKeys("500,00");
+		chrome.findElement(By.name("faixaSalarial.salarioMinimo")).sendKeys("500,00");
 		
 		//encontra o campo 'Salario Maximo' e preenche com '1000,00':
-		firefox.findElement(By.name("faixaSalarial.salarioMaximo")).sendKeys("1000,00");
+		chrome.findElement(By.name("faixaSalarial.salarioMaximo")).sendKeys("1000,00");
 		
 		//encontra o botao de gravar e clica nele
-		firefox.findElement(By.cssSelector(".btn-primary")).click();
+		chrome.findElement(By.cssSelector(".btn-primary")).click();
 		
 		//encontra a mensagem de erro do campo 'Salario Minimo':
-		WebElement msgErro = firefox.findElement(By.id("faixaSalarial.salarioMinimo.errors"));
+		WebElement msgErro = chrome.findElement(By.id("faixaSalarial.salarioMinimo.errors"));
 		
 		//verifica o texto da mensagem de erro
 		Assert.assertTrue(msgErro.getText().equals("Salário Mínimo não pode ser menor do que R$954,00"));
@@ -79,22 +79,22 @@ public class CadastroCargo {
 		logarNoSistema();
 		
 		//navega pra pagina de cadastro de cargos:
-		firefox.navigate().to(urlSistema + "/cargos/form");
+		chrome.navigate().to(urlSistema + "/cargos/form");
 		
 		//encontra o campo 'Nome' e preenche com 'Cargo teste':
-		firefox.findElement(By.name("nome")).sendKeys("Cargo teste");
+		chrome.findElement(By.name("nome")).sendKeys("Cargo teste");
 		
 		//encontra o campo 'Salario Minimo' e preenche com '3000,00':
-		firefox.findElement(By.name("faixaSalarial.salarioMinimo")).sendKeys("3000,00");
+		chrome.findElement(By.name("faixaSalarial.salarioMinimo")).sendKeys("3000,00");
 		
 		//encontra o campo 'Salario Maximo' e preenche com '2000,00':
-		firefox.findElement(By.name("faixaSalarial.salarioMaximo")).sendKeys("2000,00");
+		chrome.findElement(By.name("faixaSalarial.salarioMaximo")).sendKeys("2000,00");
 		
 		//encontra o botao de gravar e clica nele
-		firefox.findElement(By.cssSelector(".btn-primary")).click();
+		chrome.findElement(By.cssSelector(".btn-primary")).click();
 		
 		//encontra a mensagem de erro:
-		WebElement msgErro = firefox.findElement(By.cssSelector(".alert-danger"));
+		WebElement msgErro = chrome.findElement(By.cssSelector(".alert-danger"));
 		
 		//verifica o texto da mensagem de erro
 		Assert.assertTrue(msgErro.getText().equals("Salário mínimo não pode ser maior que salário máximo!"));
@@ -106,22 +106,22 @@ public class CadastroCargo {
 		logarNoSistema();
 		
 		//navega pra pagina de cadastro de cargos:
-		firefox.navigate().to(urlSistema + "/cargos/form");
+		chrome.navigate().to(urlSistema + "/cargos/form");
 		
 		//encontra o campo 'Nome' e preenche com 'Cargo teste':
-		firefox.findElement(By.name("nome")).sendKeys("Cargo teste");
+		chrome.findElement(By.name("nome")).sendKeys("Cargo teste");
 		
 		//encontra o campo 'Salario Minimo' e preenche com '3000,00':
-		firefox.findElement(By.name("faixaSalarial.salarioMinimo")).sendKeys("2000,00");
+		chrome.findElement(By.name("faixaSalarial.salarioMinimo")).sendKeys("2000,00");
 		
 		//encontra o campo 'Salario Maximo' e preenche com '2000,00':
-		firefox.findElement(By.name("faixaSalarial.salarioMaximo")).sendKeys("3000,00");
+		chrome.findElement(By.name("faixaSalarial.salarioMaximo")).sendKeys("3000,00");
 		
 		//encontra o botao de gravar e clica nele
-		firefox.findElement(By.cssSelector(".btn-primary")).click();
+		chrome.findElement(By.cssSelector(".btn-primary")).click();
 		
 		//encontra a ultima linha da tabela:
-		WebElement ultimaLinhaDaTabela = firefox.findElement(By.cssSelector(".table tbody tr:last-child"));
+		WebElement ultimaLinhaDaTabela = chrome.findElement(By.cssSelector(".table tbody tr:last-child"));
 		
 		//encontra a coluna do nome do cargo:
 		WebElement colunaNome = ultimaLinhaDaTabela.findElement(By.cssSelector("td:nth-child(2)"));
@@ -133,7 +133,7 @@ public class CadastroCargo {
 		ultimaLinhaDaTabela.findElement(By.cssSelector(".btn-excluir")).click();
 		
 		//muda o foco do browser para a popup de confirmacao de exclusao
-		Alert popupExclusao = firefox.switchTo().alert();
+		Alert popupExclusao = chrome.switchTo().alert();
 		
 		//clica no botao ok da popup de confirmacao de exclusao
 		popupExclusao.accept();
